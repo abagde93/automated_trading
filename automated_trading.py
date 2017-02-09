2b6216a021dacdd6b4b4faba36cf7d951b1907c3
abagde93 <ajinkya.abhijit.bagde@gmail.com>
2017-02-09 00:14:56 -0800

    Tested Initial State streamer. Implemented current timestamps

--- automated_trading.py
+++ automated_trading.py
@@ -1,31 +1,54 @@
 from Robinhood import Robinhood
 
 import re
+#from datetime import datetime
 import datetime
 import time
 
-#from ISStreamer.Streamer import Streamer
+from ISStreamer.Streamer import Streamer
 
 #Setup
 my_trader = Robinhood()
+
 #login
-username = input("Username: ")
-password = input("Password: ")
+username = raw_input("Username: ")
+password = raw_input("Password: ")
 my_trader.login(username, password)
 
     
-#streamer = Streamer(bucket_name="Stock Tracker", bucket_key="Stock_Tracker_020817", ini_file_location="./isstreamer.ini")
+streamer = Streamer(bucket_name="Stock Tracker", bucket_key="Stock_Tracker_020817", ini_file_location="./isstreamer.ini")
 
 
 while True:
+
+    #Get current 24HR timestamp
     ts = time.time()
-    current_time = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-     
-    if re.findall(r'[2][0-9]{1}[:][0-9]{2}[:][0-9]{2}', current_time):
-        print("peepmeep")
+    current_time = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    
+    #Look for timstamp 06:3x:xx - This is when the market opens
+    if re.findall(r'[0][6][:][3][0-9]{1}[:][0-9]{2}', current_time):
+        open_time = current_time
+        print "Market has opened"
+      
+    #Look for timstamp 13:00:xx - This is when the market closes
+    if re.findall(r'[0][6][:][0][0][:][0-9]{2}', current_time):
+        close_time = current_time
+        print "Market has closed"
+        
+    #Convert open_time and close_time to minute notation - 1440 minutes in a day
+    
+    
+    
+        
     
     print(current_time)
-    my_trader.print_quote("AMD")
+    quote = my_trader.last_trade_price("AMD")
+    print(quote)
+    #re.findall(r'\d+', quote)
+    
+    
+    streamer.log("Current AMD quote", "{value}".format(value=quote))
+    #streamer.log("Altitude", "{value}".format(value=quote))
     time.sleep(10)
 
 
--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from Robinhood import Robinhood
 #Setup
 my_trader = Robinhood();
 #login
-my_trader.login(username="abagde", password="ajinkya296541993")
+my_trader.login(username="YOUR_USERNAME", password="YOUR_PASSWORD")
 
 #Get stock information
     #Note: Sometimes more than one instrument may be returned for a given stock symbol
@@ -23,7 +23,7 @@ quote_info = my_trader.quote_data("GEVO")
 print(quote_info);
 
 #Place a buy order (uses market bid price)
-#buy_order = my_trader.place_buy_order(stock_instrument, 1)
+buy_order = my_trader.place_buy_order(stock_instrument, 1)
 
 #Place a sell order
 sell_order = my_trader.place_sell_order(stock_instrument, 1)
