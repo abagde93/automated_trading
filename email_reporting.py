96db73ccc51e44e4ca0f37466781fa97499c3f76
abagde93 <ajinkya.abhijit.bagde@gmail.com>
2017-02-16 21:27:22 -0800

    Got 30 minute loop working in email_reporting.py

--- email_reporting.py
+++ email_reporting.py
@@ -2,12 +2,14 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
+import time
+
 
 def send_message(fromaddr, toaddr, password):
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    msg['Subject'] = "Stock Report"
+    msg['Subject'] = "[Robinhood] Stock Report"
  
     body = "This is your stock report update"
     msg.attach(MIMEText(body, 'plain'))
@@ -30,4 +32,6 @@ sent_from = raw_input("What email address do you want to send from?: ")
 sent_to = raw_input("What email address do you want to send to?: ")
 email_password = raw_input("What is your email password?: ")
 
-send_message(sent_from, sent_to, email_password)
+while True:
+	send_message(sent_from, sent_to, email_password)
+	time.sleep(1800)
